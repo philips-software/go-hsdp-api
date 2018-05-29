623a58eaebf2edd05acffb5a588e6b533f9ef1f0
Andy Lo-A-Foe <andy.loafoe@aemian.com>
2018-05-29 18:10:51 +0200

    Implement GetPermissions() call

--- iam/permission.go
+++ iam/permission.go
@@ -1,30 +1,9 @@
 package iam
 
-import (
-	"encoding/json"
-
-	"github.com/jeffail/gabs"
-)
-
 type Permission struct {
 	ID          string `json:"id"`
 	Name        string `json:"name"`
 	Description string `json:"description"`
 	Category    string `json:"category"`
 	Type        string `json:"type"`
-}
-
-func (p *Permission) parseFromBundle(v interface{}) error {
-	m, _ := json.Marshal(v)
-	jsonParsed, err := gabs.ParseJSON(m)
-	if err != nil {
-		return err
-	}
-	r := jsonParsed.Path("entry").Index(0)
-	p.ID = r.Path("id").Data().(string)
-	p.Category, _ = r.Path("category").Data().(string)
-	p.Name, _ = r.Path("name").Data().(string)
-	p.Description, _ = r.Path("description").Data().(string)
-	p.Type, _ = r.Path("type").Data().(string)
-	return nil
 }
--- iam/permissions_service.go
+++ iam/permissions_service.go
@@ -1,5 +1,12 @@
 package iam
 
+import (
+	"encoding/json"
+	"errors"
+
+	"github.com/jeffail/gabs"
+)
+
 const permissionAPIVersion = "1"
 
 type PermissionsService struct {
@@ -55,7 +62,51 @@ func (p *PermissionsService) GetPermission(opt *GetPermissionOptions, options ..
 	if err != nil {
 		return nil, resp, err
 	}
-	var permission Permission
-	err = permission.parseFromBundle(bundleResponse)
-	return &permission, resp, err
+	permissions, err := p.parseFromBundle(bundleResponse)
+	if err != nil {
+		return nil, resp, err
+	}
+	return &(*permissions)[0], resp, nil
+}
+
+func (p *PermissionsService) GetPermissions(opt GetPermissionOptions, options ...OptionFunc) (*[]Permission, *Response, error) {
+	req, err := p.client.NewIDMRequest("GET", "authorize/identity/Permission", opt, options)
+	if err != nil {
+		return nil, nil, err
+	}
+	req.Header.Set("api-version", permissionAPIVersion)
+
+	var bundleResponse interface{}
+
+	resp, err := p.client.DoSigned(req, &bundleResponse)
+	if err != nil {
+		return nil, resp, err
+	}
+	permissions, err := p.parseFromBundle(bundleResponse)
+	return permissions, resp, err
+}
+
+func (p *PermissionsService) parseFromBundle(bundleResponse interface{}) (*[]Permission, error) {
+	m, _ := json.Marshal(bundleResponse)
+	jsonParsed, err := gabs.ParseJSON(m)
+	if err != nil {
+		return nil, err
+	}
+	count, ok := jsonParsed.S("total").Data().(float64)
+	if !ok || count == 0 {
+		return nil, errors.New("empty result")
+	}
+	permissions := make([]Permission, int64(count))
+
+	children, _ := jsonParsed.S("entry").Children()
+	for i, r := range children {
+		var p Permission
+		p.ID = r.Path("id").Data().(string)
+		p.Category, _ = r.Path("category").Data().(string)
+		p.Name, _ = r.Path("name").Data().(string)
+		p.Description, _ = r.Path("description").Data().(string)
+		p.Type, _ = r.Path("type").Data().(string)
+		permissions[i] = p
+	}
+	return &permissions, nil
 }
