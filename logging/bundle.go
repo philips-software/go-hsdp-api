0dde02bc00c9b47eb2e8f6bd8d9105f3df2d2cad
Andy Lo-A-Foe <andy.loafoe@aemian.com>
2018-06-23 13:23:46 +0200

    Document code

--- logging/bundle.go
+++ logging/bundle.go
@@ -1,5 +1,7 @@
 package logging
 
+// Bundle is a FHIR bundle resource
+// There is just enough there to create the logging payload
 type Bundle struct {
 	ResourceType string    `json:"resourceType"`
 	Type         string    `json:"type"`
@@ -8,10 +10,12 @@ type Bundle struct {
 	Entry        []Element `json:"entry"`
 }
 
+// Element is a FHIR element resource
 type Element struct {
 	Resource Resource `json:"resource"`
 }
 
+// Resource is a logging resource
 type Resource struct {
 	ResourceType        string  `json:"resourceType"`        // LogEvent
 	ID                  string  `json:"id"`                  // 7f4c85a8-e472-479f-b772-2916353d02a4
@@ -30,13 +34,15 @@ type Resource struct {
 	LogData             LogData `json:"logData"`
 }
 
+// LogData is the payload of a log message
+type LogData struct {
+	Message string `json:"message"`
+}
+
+// Valid returns true if a resource is valid according to HSDP rules, false otherwise
 func (r *Resource) Valid() bool {
 	if r.EventID != "" && r.TransactionID != "" && r.LogTime != "" && r.LogData.Message != "" {
 		return true
 	}
 	return false
-}
-
-type LogData struct {
-	Message string `json:"message"`
 }
