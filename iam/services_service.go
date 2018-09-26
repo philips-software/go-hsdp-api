5341c09ecd1b50defb7894d91398c9931e3fc17d
Andy Lo-A-Foe <andy.loafoe@aemian.com>
2018-09-26 10:04:29 +0200

    Add support for scopes and defaultScopes

--- iam/services_service.go
+++ iam/services_service.go
@@ -12,14 +12,16 @@ const servicesAPIVersion = "1"
 
 // Service represents a IAM service resource
 type Service struct {
-	ID             string `json:"id,omitempty"`
-	Name           string `json:"name"`
-	ServiceID      string `json:"serviceId,omitempty"`
-	Description    string `json:"description"`
-	ApplicationID  string `json:"applicationId"`
-	OrganizationID string `json:"organizationId,omitempty"`
-	ExpiresOn      string `json:"expiresOn,omitempty"`
-	PrivateKey     string `json:"privateKey,omitempty"`
+	ID             string   `json:"id,omitempty"`
+	Name           string   `json:"name"`
+	Description    string   `json:"description"`
+	ApplicationID  string   `json:"applicationId"`
+	ServiceID      string   `json:"serviceId,omitempty"`
+	OrganizationID string   `json:"organizationId,omitempty"`
+	ExpiresOn      string   `json:"expiresOn,omitempty"`
+	PrivateKey     string   `json:"privateKey,omitempty"`
+	Scopes         []string `json:"scopes,omitempty"`
+	DefaultScopes  []string `json:"defaultScopes,omitempty"`
 }
 
 // ServicesService provides operations on IAM Sessions resources
@@ -33,6 +35,7 @@ type GetServiceOptions struct {
 	Name           *string `url:"name,omitempty"`
 	ApplicationID  *string `url:"applicationId,omitempty"`
 	OrganizationID *string `url:"organizationId,omitempty"`
+	ServiceID      *string `url:"serviceId,omitempty"`
 }
 
 // GetServiceByID looks up a service by ID
@@ -77,6 +80,7 @@ func (p *ServicesService) CreateService(name, description, applicationID string)
 	}
 	req, err := p.client.NewRequest(IDM, "POST", "authorize/identity/Service", role, nil)
 	req.Header.Set("api-version", servicesAPIVersion)
+	req.Header.Set("Content-Type", "application/json")
 
 	var createdService Service
 
@@ -103,6 +107,7 @@ func (p *ServicesService) GetServices(opt *GetServiceOptions, options ...OptionF
 		return nil, nil, err
 	}
 	req.Header.Set("api-version", servicesAPIVersion)
+	req.Header.Set("Content-Type", "application/json")
 
 	var bundleResponse bytes.Buffer
 
@@ -121,6 +126,7 @@ func (p *ServicesService) DeleteService(service Service) (bool, *Response, error
 		return false, nil, err
 	}
 	req.Header.Set("api-version", servicesAPIVersion)
+	req.Header.Set("Content-Type", "application/json")
 
 	var deleteResponse interface{}
 
@@ -129,6 +135,37 @@ func (p *ServicesService) DeleteService(service Service) (bool, *Response, error
 		return false, resp, nil
 	}
 	return true, resp, err
+}
+
+// UpdateScopes updates a service scope
+func (p *ServicesService) UpdateScopes(service Service, scopes []string, defaultScopes []string) (bool, *Response, error) {
+	// TODO: simulate update using ["add", "remove"] action. Currently we only "add"
+	var requestBody = struct {
+		Action        string   `json:"action"`
+		Scopes        []string `json:"scopes"`
+		DefaultScopes []string `json:"defaultScopes"`
+	}{
+		"add",
+		scopes,
+		defaultScopes,
+	}
+	req, err := p.client.NewRequest(IDM, "PUT", "authorize/identity/Service/"+service.ID+"/$scopes", requestBody, nil)
+	if err != nil {
+		return false, nil, err
+	}
+	req.Header.Set("api-version", servicesAPIVersion)
+	req.Header.Set("Content-Type", "application/json")
+
+	var putResponse bytes.Buffer
+
+	resp, err := p.client.Do(req, &putResponse)
+	if err != nil {
+		return false, resp, err
+	}
+	if resp.StatusCode != http.StatusNoContent {
+		return false, resp, errOperationFailed
+	}
+	return true, resp, nil
 }
 
 func (p *ServicesService) parseFromBundle(bundle []byte) (*[]Service, error) {
@@ -153,6 +190,14 @@ func (p *ServicesService) parseFromBundle(bundle []byte) (*[]Service, error) {
 		s.ApplicationID, _ = r.Path("applicationId").Data().(string)
 		s.PrivateKey, _ = r.Path("privateKey").Data().(string)
 		s.ExpiresOn, _ = r.Path("expiresOn").Data().(string)
+		children, _ = r.Path("scopes").Children()
+		for _, child := range children {
+			s.Scopes = append(s.Scopes, child.Data().(string))
+		}
+		children, _ = r.Path("defaultScopes").Children()
+		for _, child := range children {
+			s.DefaultScopes = append(s.DefaultScopes, child.Data().(string))
+		}
 		services[i] = s
 	}
 	return &services, nil
