191b1b3fa6bd9c59f014b9bf6855187035d49ade
Andy Lo-A-Foe <andy.loafoe@aemian.com>
2018-11-02 06:16:55 +0100

    Clean up client code a bit

--- tpns/client.go
+++ tpns/client.go
@@ -170,14 +170,6 @@ type Response struct {
 	*http.Response
 }
 
-// ErrorResponse represents an IAM errors response
-// containing a code and a human readable message
-type ErrorResponse struct {
-	Response *http.Response `json:"-"`
-	Code     string         `json:"responseCode"`
-	Message  string         `json:"responseMessage"`
-}
-
 // newResponse creates a new Response for the provided http.Response.
 func newResponse(r *http.Response) *Response {
 	response := &Response{Response: r}
@@ -214,7 +206,7 @@ func (c *Client) Do(req *http.Request, v interface{}) (*Response, error) {
 
 	response := newResponse(resp)
 
-	err = CheckResponse(resp)
+	err = fhir.CheckResponse(resp)
 	if err != nil {
 		// even though there was an error, we still return the response
 		// in case the caller wants to inspect it further
@@ -230,31 +222,4 @@ func (c *Client) Do(req *http.Request, v interface{}) (*Response, error) {
 	}
 
 	return response, err
-}
-
-func (e *ErrorResponse) Error() string {
-	path, _ := url.QueryUnescape(e.Response.Request.URL.Opaque)
-	u := fmt.Sprintf("%s://%s%s", e.Response.Request.URL.Scheme, e.Response.Request.URL.Host, path)
-	return fmt.Sprintf("%s %s: %d %s", e.Response.Request.Method, u, e.Response.StatusCode, e.Message)
-}
-
-// CheckResponse checks the API response for errors, and returns them if present.
-func CheckResponse(r *http.Response) error {
-	switch r.StatusCode {
-	case 200, 201, 202, 204, 304:
-		return nil
-	}
-
-	errorResponse := &ErrorResponse{Response: r}
-	data, err := ioutil.ReadAll(r.Body)
-	if err == nil && data != nil {
-		var raw interface{}
-		if err := json.Unmarshal(data, &raw); err != nil {
-			errorResponse.Message = "failed to parse unknown error format"
-		}
-
-		errorResponse.Message = fhir.ParseError(raw)
-	}
-
-	return errorResponse
 }
