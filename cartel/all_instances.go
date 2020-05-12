76295b054846abd8ea73cac83a9e24ade183cd4e
Andy Lo-A-Foe <andy.lo-a-foe@philips.com>
2020-05-12 06:04:20 +0200

    Add GetAllInstances()

---
+++ cartel/all_instances.go
@@ -0,0 +1,16 @@
+package cartel
+
+func (c *Client) GetAllInstances() (*[]InstanceDetails, *Response, error) {
+	var body CartelRequestBody
+
+	req, err := c.NewRequest("POST", "v3/api/get_all_instances", &body, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var responseBody []InstanceDetails
+
+	resp, err := c.Do(req, &responseBody)
+
+	return &responseBody, resp, err
+}
