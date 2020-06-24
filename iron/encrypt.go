d997999b8a3ebdd2c8672ce8129361040a3f7f9b
Andy Lo-A-Foe <andy.lo-a-foe@philips.com>
2020-06-24 13:01:50 +0200

    Add encryption helpers from siderite project

---
+++ iron/encrypt.go
@@ -0,0 +1,86 @@
+package iron
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha1"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/pem"
+	"fmt"
+	"strings"
+)
+
+const (
+	keyHeader = "-----BEGIN PUBLIC KEY-----"
+	keyFooter = "-----END PUBLIC KEY-----"
+)
+
+//EncryptPayload encrypts pbytes using publicKey
+func EncryptPayload(publicKey []byte, pbytes []byte) (string, error) {
+	rsaPublicKey, err := parsePublicKey(publicKey)
+	if err != nil {
+		return "", err
+	}
+
+	// get a random aes-128 session key to encrypt
+	aesKey := make([]byte, 128/8)
+	if _, err := rand.Read(aesKey); err != nil {
+		return "", err
+	}
+	// have to use sha1 b/c ruby openssl picks it for OAEP:  https://www.openssl.org/docs/manmaster/crypto/RSA_public_encrypt.html
+	aesKeyCipher, err := rsa.EncryptOAEP(sha1.New(), rand.Reader, rsaPublicKey, aesKey, nil)
+	if err != nil {
+		return "", err
+	}
+	block, err := aes.NewCipher(aesKey)
+	if err != nil {
+		return "", err
+	}
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+	// The IV needs to be unique, but not secure. last 12 bytes are IV.
+	ciphertext := make([]byte, len(pbytes)+gcm.Overhead()+gcm.NonceSize())
+	nonce := ciphertext[len(ciphertext)-gcm.NonceSize():]
+	if _, err := rand.Read(nonce); err != nil {
+		return "", err
+	}
+	// tag is appended to cipher as last 16 bytes. https://golang.org/src/crypto/cipher/gcm.go?s=2318:2357#L145
+	gcm.Seal(ciphertext[:0], nonce, pbytes, nil)
+	// base64 the whole thing
+	payload := base64.StdEncoding.EncodeToString(append(aesKeyCipher, ciphertext...))
+	return payload, nil
+}
+
+func parsePublicKey(pubkey []byte) (key *rsa.PublicKey, err error) {
+	defer func() {
+		if errr := recover(); errr != nil {
+			key = nil
+			err = fmt.Errorf("panic during decode")
+		}
+	}()
+	fixed := FormatBrokenPubkey(pubkey)
+	rsablock, _ := pem.Decode(fixed)
+
+	rsaKey, err := x509.ParsePKIXPublicKey(rsablock.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	rsaPublicKey, ok := rsaKey.(*rsa.PublicKey)
+	if !ok {
+		return nil, fmt.Errorf("not a RSA public key")
+	}
+	return rsaPublicKey, nil
+}
+
+// FormatBrokenPubkey fixes to broken service broker pubkey format
+func FormatBrokenPubkey(pubkey []byte) []byte {
+	a := strings.Replace(string(pubkey), keyHeader, "", 1)
+	b := strings.Replace(a, keyFooter, "", 1)
+	c := strings.ReplaceAll(b, " ", "\n")
+	return []byte(keyHeader + c + keyFooter)
+}
---
+++ iron/encrypt_test.go
@@ -0,0 +1,36 @@
+package iron
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFormatBrokenPubKey(t *testing.T) {
+	pubkey := []byte("-----BEGIN PUBLIC KEY----- MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCdS2oE9+dhexZc3/sEtI+a6ZKt 6FwBZaAgytdkQ7sX4FwbZAdJ7zFS1m0gDezyFTBJSPVjYOKYr0fu1ao/xkNkKnnz J2WkW6qsDNKwJgrHiCO1asnoW5XWtk8Yc4kKkg63REuV20x+QoD6onTCo3T2DfUI vZ8QOSJQ7NotGuO2wwIDAQAB -----END PUBLIC KEY-----")
+	fixedPubkey := []byte(`-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCdS2oE9+dhexZc3/sEtI+a6ZKt
+6FwBZaAgytdkQ7sX4FwbZAdJ7zFS1m0gDezyFTBJSPVjYOKYr0fu1ao/xkNkKnnz
+J2WkW6qsDNKwJgrHiCO1asnoW5XWtk8Yc4kKkg63REuV20x+QoD6onTCo3T2DfUI
+vZ8QOSJQ7NotGuO2wwIDAQAB
+-----END PUBLIC KEY-----`)
+
+	fixed := FormatBrokenPubkey(pubkey)
+	assert.Equal(t, fixedPubkey, fixed)
+
+	noop := FormatBrokenPubkey(fixed)
+	assert.Equal(t, fixedPubkey, noop)
+}
+
+func TestEncryptPayload(t *testing.T) {
+	pubkey := []byte(`-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCdS2oE9+dhexZc3/sEtI+a6ZKt
+6FwBZaAgytdkQ7sX4FwbZAdJ7zFS1m0gDezyFTBJSPVjYOKYr0fu1ao/xkNkKnnz
+J2WkW6qsDNKwJgrHiCO1asnoW5XWtk8Yc4kKkg63REuV20x+QoD6onTCo3T2DfUI
+vZ8QOSJQ7NotGuO2wwIDAQAB
+-----END PUBLIC KEY-----`)
+	fixed := FormatBrokenPubkey(pubkey)
+	encrypted, err := EncryptPayload([]byte(fixed), []byte("Yo"))
+	assert.Nil(t, err)
+	assert.Equal(t, 212, len(encrypted))
+}
