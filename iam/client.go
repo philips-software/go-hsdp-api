c951ce867a830cd9603135ea8ec10b4964296680
Andy Lo-A-Foe <andy.lo-a-foe@philips.com>
2020-07-02 13:08:44 +0200

    Extract autoconf in method

--- iam/client.go
+++ iam/client.go
@@ -122,21 +122,7 @@ func newClient(httpClient *http.Client, config *Config) (*Client, error) {
 	if httpClient == nil {
 		httpClient = http.DefaultClient
 	}
-	// Autoconfig
-	if config.Region != "" && config.Environment != "" {
-		c, err := autoconf.New(
-			autoconf.WithRegion(config.Region),
-			autoconf.WithEnv(config.Environment))
-		if err == nil {
-			iamService := c.Service("iam")
-			if iamURL, err := iamService.GetString("iam_url"); err == nil && config.IAMURL == "" {
-				config.IAMURL = iamURL
-			}
-			if idmURL, err := iamService.GetString("idm_url"); err == nil && config.IDMURL == "" {
-				config.IDMURL = idmURL
-			}
-		}
-	}
+	doAutoconf(config)
 	c := &Client{client: httpClient, config: config, UserAgent: userAgent}
 	if err := c.SetBaseIAMURL(c.config.IAMURL); err != nil {
 		return nil, err
@@ -174,6 +160,23 @@ func newClient(httpClient *http.Client, config *Config) (*Client, error) {
 	c.Services = &ServicesService{client: c}
 	c.MFAPolicies = &MFAPoliciesService{client: c, validate: validator.New()}
 	return c, nil
+}
+
+func doAutoconf(config *Config) {
+	if config.Region != "" && config.Environment != "" {
+		c, err := autoconf.New(
+			autoconf.WithRegion(config.Region),
+			autoconf.WithEnv(config.Environment))
+		if err == nil {
+			iamService := c.Service("iam")
+			if iamURL, err := iamService.GetString("iam_url"); err == nil && config.IAMURL == "" {
+				config.IAMURL = iamURL
+			}
+			if idmURL, err := iamService.GetString("idm_url"); err == nil && config.IDMURL == "" {
+				config.IDMURL = idmURL
+			}
+		}
+	}
 }
 
 func (c *Client) validSigner() bool {
