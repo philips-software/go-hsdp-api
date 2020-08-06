c996bb17e1c275072ced06f9789706f94e33d2c4
Andy Lo-A-Foe <andy.lo-a-foe@philips.com>
2020-08-06 18:04:03 +0200

    Password policies management

---
+++ iam/password_policies_service.go
@@ -0,0 +1,145 @@
+package iam
+
+import (
+	"bytes"
+	"net/http"
+
+	"github.com/go-playground/validator/v10"
+)
+
+const (
+	PasswordPolicyAPIVersion = "1"
+)
+
+type PasswordPoliciesService struct {
+	client   *Client
+	validate *validator.Validate
+}
+
+type PasswordPolicy struct {
+	ID                   string `json:"id,omitempty"`
+	ManagingOrganization string `json:"managingOrganization"`
+	ExpiryPeriodInDays   int    `json:"expiryPeriodInDays"`
+	HistoryCount         int    `json:"historyCount"`
+	Complexity           struct {
+		MinLength       int `json:"minLength"`
+		MaxLength       int `json:"maxLength"`
+		MinNumerics     int `json:"minNumerics"`
+		MinUpperCase    int `json:"minUpperCase"`
+		MinLowerCase    int `json:"minLowerCase"`
+		MinSpecialChars int `json:"minSpecialChars"`
+	} `json:"complexity"`
+	ChallengesEnabled bool `json:"challengesEnabled"`
+	ChallengePolicy   struct {
+		DefaultQuestions     []string `json:"defaultQuestions"`
+		MinQuestionCount     int      `json:"minQuestionCount"`
+		MinAnswerCount       int      `json:"minAnswerCount"`
+		MaxIncorrectAttempts int      `json:"maxIncorrectAttempts"`
+	} `json:"challengePolicy,omitempty"`
+	Meta *Meta `json:"meta,omitempty"`
+}
+
+// GetPasswordPolicyByID retrieves a Password policy by ID
+func (p *PasswordPoliciesService) GetPasswordPolicyByID(id string) (*PasswordPolicy, *Response, error) {
+	req, err := p.client.NewRequest(IDM, "GET", "authorize/identity/PasswordPolicy/"+id, nil, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+	req.Header.Set("api-version", PasswordPolicyAPIVersion)
+	req.Header.Set("Content-Type", "application/json")
+
+	var policy PasswordPolicy
+
+	resp, err := p.client.Do(req, &policy)
+	if err != nil {
+		return nil, resp, err
+	}
+	if policy.ID != id {
+		return nil, resp, ErrNotFound
+	}
+	return &policy, resp, err
+}
+
+// UpdatePasswordPolicy updates a password policy
+func (p *PasswordPoliciesService) UpdatePasswordPolicy(policy *PasswordPolicy) (*PasswordPolicy, *Response, error) {
+
+	req, _ := p.client.NewRequest(IDM, "PUT", "authorize/identity/PasswordPolicy/"+policy.ID, policy, nil)
+	req.Header.Set("api-version", PasswordPolicyAPIVersion)
+	req.Header.Set("Content-Type", "application/json")
+	if policy.Meta == nil {
+		return nil, nil, ErrMissingEtagInformation
+	}
+	req.Header.Set("If-Match", policy.Meta.Version)
+
+	var updatedPolicy PasswordPolicy
+	resp, err := p.client.Do(req, &updatedPolicy)
+
+	if err != nil {
+		return nil, resp, err
+	}
+	return &updatedPolicy, resp, nil
+
+}
+
+// CreatePasswordPolicy creates a password policy
+func (p *PasswordPoliciesService) CreatePasswordPolicy(policy PasswordPolicy) (*PasswordPolicy, *Response, error) {
+	if err := p.validate.Struct(policy); err != nil {
+		return nil, nil, err
+	}
+	req, _ := p.client.NewRequest(IDM, "POST", "authorize/identity/PasswordPolicy", &policy, nil)
+	req.Header.Set("api-version", PasswordPolicyAPIVersion)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
+
+	var createdPolicy PasswordPolicy
+
+	resp, err := p.client.Do(req, &createdPolicy)
+	if err != nil {
+		return nil, resp, err
+	}
+	return &createdPolicy, resp, err
+}
+
+// DeletePasswordPolicy deletes the given password policy
+func (p *PasswordPoliciesService) DeletePasswordPolicy(policy PasswordPolicy) (bool, *Response, error) {
+	req, err := p.client.NewRequest(IDM, "DELETE", "authorize/identity/PasswordPolicy/"+policy.ID, nil, nil)
+	if err != nil {
+		return false, nil, err
+	}
+	req.Header.Set("api-version", PasswordPolicyAPIVersion)
+	req.Header.Set("Content-Type", "application/json")
+
+	var deleteResponse bytes.Buffer
+
+	resp, err := p.client.Do(req, &deleteResponse)
+	if resp == nil || resp.StatusCode != http.StatusNoContent {
+		return false, resp, nil
+	}
+	return true, resp, err
+}
+
+// GetPasswordPolicyOptions describes the criteria for looking up password polices
+type GetPasswordPolicyOptions struct {
+	OrganizationID *string `url:"organizationId,omitempty"`
+}
+
+// GetPasswordPolicies looks up clients based on GetClientsOptions
+func (p *PasswordPoliciesService) GetPasswordPolicies(opt *GetPasswordPolicyOptions, options ...OptionFunc) (*[]PasswordPolicy, *Response, error) {
+	req, err := p.client.NewRequest(IDM, "GET", "authorize/identity/PasswordPolicy", opt, options)
+	if err != nil {
+		return nil, nil, err
+	}
+	req.Header.Set("api-version", PasswordPolicyAPIVersion)
+	req.Header.Set("Content-Type", "application/json")
+
+	var bundleResponse struct {
+		Total int              `json:"total"`
+		Entry []PasswordPolicy `json:"entry"`
+	}
+
+	resp, err := p.client.Do(req, &bundleResponse)
+	if err != nil {
+		return nil, resp, err
+	}
+	return &bundleResponse.Entry, resp, err
+}
---
+++ iam/password_policies_service_test.go
@@ -0,0 +1,162 @@
+package iam
+
+import (
+	"io"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPasswordPolicyCrud(t *testing.T) {
+	teardown := setup(t)
+	defer teardown()
+
+	id := "2c266886-f918-4223-941d-437cb3cd09e8"
+	orgID := "bda40124-54fa-4967-b2fb-23dcc4e0ad1a"
+
+	muxIDM.HandleFunc("/authorize/identity/PasswordPolicy", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		switch r.Method {
+		case "POST":
+			w.WriteHeader(http.StatusCreated)
+			_, _ = io.WriteString(w, `{
+      "managingOrganization": "`+orgID+`",
+      "expiryPeriodInDays": 720,
+      "historyCount": 5,
+      "complexity": {
+        "minLength": 8,
+        "maxLength": 16,
+        "minNumerics": 1,
+        "minUpperCase": 1,
+        "minLowerCase": 1,
+        "minSpecialChars": 1
+      },
+      "id": "`+id+`",
+      "meta": {
+        "version": "W/\"233552990\"",
+        "updatedBy": "29c58b06-fdaa-461f-afef-53c91a18acbd",
+        "createdBy": "29c58b06-fdaa-461f-afef-53c91a18acbd",
+        "created": "2020-04-23T19:40:31.463Z",
+        "lastModified": "2020-04-24T20:47:27.473Z"
+      }
+    }`)
+		case "GET":
+			w.WriteHeader(http.StatusOK)
+			_, _ = io.WriteString(w, `{
+				"total": 1,
+				"entry": [ {
+      "managingOrganization": "`+orgID+`",
+      "expiryPeriodInDays": 720,
+      "historyCount": 5,
+      "complexity": {
+        "minLength": 8,
+        "maxLength": 16,
+        "minNumerics": 1,
+        "minUpperCase": 1,
+        "minLowerCase": 1,
+        "minSpecialChars": 1
+      },
+      "id": "`+id+`",
+      "meta": {
+        "version": "W/\"233552990\"",
+        "updatedBy": "29c58b06-fdaa-461f-afef-53c91a18acbd",
+        "createdBy": "29c58b06-fdaa-461f-afef-53c91a18acbd",
+        "created": "2020-04-23T19:40:31.463Z",
+        "lastModified": "2020-04-24T20:47:27.473Z"
+      }
+    }
+]
+	}`)
+		}
+	})
+	muxIDM.HandleFunc("/authorize/identity/PasswordPolicy/"+id, func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		switch r.Method {
+		case "DELETE":
+			w.WriteHeader(http.StatusNoContent)
+		case "GET", "PUT":
+			w.WriteHeader(http.StatusOK)
+			_, _ = io.WriteString(w, `{
+      "managingOrganization": "`+orgID+`",
+      "expiryPeriodInDays": 720,
+      "historyCount": 5,
+      "complexity": {
+        "minLength": 8,
+        "maxLength": 16,
+        "minNumerics": 1,
+        "minUpperCase": 1,
+        "minLowerCase": 1,
+        "minSpecialChars": 1
+      },
+      "id": "`+id+`",
+      "meta": {
+        "version": "W/\"233552990\"",
+        "updatedBy": "29c58b06-fdaa-461f-afef-53c91a18acbd",
+        "createdBy": "29c58b06-fdaa-461f-afef-53c91a18acbd",
+        "created": "2020-04-23T19:40:31.463Z",
+        "lastModified": "2020-04-24T20:47:27.473Z"
+      }
+    }`)
+
+		}
+	})
+
+	var p PasswordPolicy
+	p.ManagingOrganization = orgID
+	p.ExpiryPeriodInDays = 90
+	p.HistoryCount = 10
+
+	policy, resp, err := client.PasswordPolicies.CreatePasswordPolicy(p)
+	if ok := assert.Nil(t, err); !ok {
+		return
+	}
+	if ok := assert.NotNil(t, resp); !ok {
+		return
+	}
+	if ok := assert.NotNil(t, policy); !ok {
+		return
+	}
+	assert.Equal(t, http.StatusCreated, resp.StatusCode)
+	assert.Equal(t, id, policy.ID)
+
+	foundPolicy, resp, err := client.PasswordPolicies.GetPasswordPolicyByID(policy.ID)
+	if !assert.NotNil(t, resp) {
+		return
+	}
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	if !assert.NotNil(t, foundPolicy) {
+		return
+	}
+	assert.Equal(t, policy.ID, foundPolicy.ID)
+
+	policies, resp, err := client.PasswordPolicies.GetPasswordPolicies(&GetPasswordPolicyOptions{
+		OrganizationID: &orgID,
+	})
+	if !assert.NotNil(t, resp) {
+		return
+	}
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	if !assert.NotNil(t, policies) {
+		return
+	}
+	assert.Equal(t, 1, len(*policies))
+
+	updatedPolicy, resp, err := client.PasswordPolicies.UpdatePasswordPolicy(foundPolicy)
+	if !assert.NotNil(t, resp) {
+		return
+	}
+	if !assert.NotNil(t, updatedPolicy) {
+		return
+	}
+
+	ok, resp, err := client.PasswordPolicies.DeletePasswordPolicy(*foundPolicy)
+	if !assert.NotNil(t, resp) {
+		return
+	}
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusNoContent, resp.StatusCode)
+	assert.True(t, ok)
+}
