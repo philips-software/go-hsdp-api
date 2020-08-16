7d0dd3c32c2f61f1ae77821d4c4da2c00f5413e7
Andy Lo-A-Foe <andy.lo-a-foe@philips.com>
2020-08-16 19:17:01 +0200

    Fix logging auto config

--- logging/client.go
+++ logging/client.go
@@ -118,7 +118,8 @@ func NewClient(httpClient *http.Client, config *Config) (*Client, error) {
 			autoconf.WithEnv(config.Environment))
 		if err == nil {
 			loggingService := c.Service("logging")
-			if ingestorURL, err := loggingService.GetString("ingestor_url"); err == nil && config.BaseURL == "" {
+			if ingestorURL, err := loggingService.GetString("url" +
+				""); err == nil && config.BaseURL == "" {
 				config.BaseURL = ingestorURL
 			}
 		}
