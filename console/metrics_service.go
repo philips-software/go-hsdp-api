bd7ce4238e9c345fe569a232fbd72f6f1ca3fefe
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2020-08-30 21:05:56 +0200

    Add rule retrieval methods

--- console/metrics_service.go
+++ console/metrics_service.go
@@ -21,6 +21,90 @@ type MetricsResponse struct {
 	Status string `json:"status"`
 }
 
+type Group struct {
+	Name  string `json:"name"`
+	Rules []Rule `json:"rules"`
+}
+
+type RuleResponse struct {
+	Data struct {
+		Groups []Group `json:"groups"`
+	} `json:"data"`
+	Status string `json:"status"`
+}
+
+type Rule struct {
+	Annotations struct {
+		Description string `json:"description"`
+		Resolved    string `json:"resolved"`
+		Summary     string `json:"summary"`
+	} `json:"annotations"`
+	Description string `json:"description"`
+	ID          string `json:"id"`
+	Metric      string `json:"metric"`
+	Rule        struct {
+		ExtraFor []struct {
+			Name         string   `json:"name"`
+			Options      []string `json:"options"`
+			Type         string   `json:"type"`
+			VariableName string   `json:"variableName"`
+		} `json:"extraFor"`
+		Extras []struct {
+			Name         string   `json:"name"`
+			Options      []string `json:"options"`
+			Type         string   `json:"type"`
+			VariableName string   `json:"variableName"`
+		} `json:"extras"`
+		Operators []string `json:"operators"`
+		Subject   string   `json:"subject"`
+		Threshold struct {
+			Default int      `json:"default"`
+			Max     int      `json:"max"`
+			Min     int      `json:"min"`
+			Type    string   `json:"type"`
+			Unit    []string `json:"unit"`
+		} `json:"threshold"`
+	} `json:"rule"`
+	Template string `json:"template"`
+}
+
+// GetGroupedRules looks up available rules
+func (c *MetricsService) GetGroupedRules(options ...OptionFunc) (*[]Group, *Response, error) {
+	req, err := c.client.NewRequest(CONSOLE, "GET", "v3/metrics/rules", nil, options)
+	if err != nil {
+		return nil, nil, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	var response RuleResponse
+
+	resp, err := c.client.Do(req, &response)
+	if err != nil {
+		return nil, resp, err
+	}
+	return &response.Data.Groups, resp, err
+}
+
+// GetRuleByID looks up available instances
+func (c *MetricsService) GetRuleByID(id string, options ...OptionFunc) (*Rule, *Response, error) {
+	req, err := c.client.NewRequest(CONSOLE, "GET", "v3/metrics/rules/"+id, nil, options)
+	if err != nil {
+		return nil, nil, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	var response struct {
+		Data   Rule   `json:"data"`
+		Status string `json:"status"`
+	}
+
+	resp, err := c.client.Do(req, &response)
+	if err != nil {
+		return nil, resp, err
+	}
+	return &response.Data, resp, err
+}
+
 // GetInstances looks up available instances
 func (c *MetricsService) GetInstances(options ...OptionFunc) (*[]Instance, *Response, error) {
 	req, err := c.client.NewRequest(CONSOLE, "GET", "v3/metrics/instances", nil, options)
@@ -38,7 +122,7 @@ func (c *MetricsService) GetInstances(options ...OptionFunc) (*[]Instance, *Resp
 	return &response.Data.Instances, resp, err
 }
 
-// GetInstances looks up available instances
+// GetInstanceByID looks up an instance by ID
 func (c *MetricsService) GetInstanceByID(id string, options ...OptionFunc) (*Instance, *Response, error) {
 	req, err := c.client.NewRequest(CONSOLE, "GET", "v3/metrics/intances/"+id, nil, options)
 	if err != nil {
