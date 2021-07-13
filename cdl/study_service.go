df195e2912c24ab9c9b58f83ef01f3b0f4f8d8e0
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-07-13 12:08:06 +0200

    Add GetStudies
    Signed-off-by: Andy Lo-A-Foe <andy.loafoe@gmail.com>

--- cdl/study_service.go
+++ cdl/study_service.go
@@ -3,6 +3,7 @@ package cdl
 import (
 	"fmt"
 	"io"
+	"net/http"
 
 	"github.com/go-playground/validator/v10"
 )
@@ -24,6 +25,10 @@ type StudyService struct {
 	validate *validator.Validate
 }
 
+// GetOptions describes the fields on which you can search for studies
+type GetOptions struct {
+}
+
 func (s *StudyService) path(remainder string) string {
 	return fmt.Sprintf("store/cdl/%s/%s", s.config.OrganizationID, remainder)
 }
@@ -36,6 +41,8 @@ func (s *StudyService) CreateStudy(study Study) (*Study, *Response, error) {
 	if err != nil {
 		return nil, nil, err
 	}
+	req.Header.Set("Api-Version", "2")
+
 	var createdStudy Study
 	resp, err := s.client.do(req, &createdStudy)
 	if (err != nil && err != io.EOF) || resp == nil {
@@ -45,4 +52,35 @@ func (s *StudyService) CreateStudy(study Study) (*Study, *Response, error) {
 		return nil, resp, err
 	}
 	return &createdStudy, resp, nil
+}
+
+func (s *StudyService) GetStudies(opt *GetOptions, options ...OptionFunc) ([]Study, *Response, error) {
+	var studies []Study
+
+	req, err := s.client.newCDLRequest("GET", s.path("Study"), opt, options...)
+	if err != nil {
+		return nil, nil, err
+	}
+	req.Header.Set("Api-Version", "3")
+
+	var bundleResponse struct {
+		ResourceType string  `json:"resourceType,omitempty"`
+		Type         string  `json:"type,omitempty"`
+		Total        int     `json:"total"`
+		Entry        []Study `json:"entry"`
+	}
+	resp, err := s.client.do(req, &bundleResponse)
+	if err != nil {
+		if resp != nil && resp.StatusCode == http.StatusNotFound {
+			return nil, resp, ErrEmptyResult
+		}
+		return nil, resp, err
+	}
+	if bundleResponse.Total == 0 {
+		return studies, resp, ErrEmptyResult
+	}
+	for _, e := range bundleResponse.Entry {
+		studies = append(studies, e)
+	}
+	return studies, resp, err
 }
