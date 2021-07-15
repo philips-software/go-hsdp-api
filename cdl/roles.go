2120b479d1dc4ce8b90259f7e861622a172cb811
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-07-15 12:50:32 +0200

    Add missing file
    Signed-off-by: Andy Lo-A-Foe <andy.loafoe@gmail.com>

---
+++ cdl/roles.go
@@ -0,0 +1,37 @@
+package cdl
+
+const (
+	ROLE_STUDYMANAGER   = "STUDYMANAGER"
+	ROLE_MONITOR        = "MONITOR"
+	ROLE_UPLOADER       = "UPLOADER"
+	ROLE_DATA_SCIENTIST = "DATASCIENTIST"
+)
+
+type RoleRequest struct {
+	IAMUserUUID string `json:"IAMuserUUID" validate:"required"`
+	Email       string `json:"email" validate:"required"`
+	Role        string `json:"role" validate:"required"`
+	InstituteID string `json:"instituteID"`
+}
+
+type RoleAssignment struct {
+	IAMUserUUID string `json:"IAMuserUUID"`
+	Roles       []struct {
+		Role string `json:"role"`
+	} `json:"roles"`
+}
+
+type RoleAssignmentResult []RoleAssignment
+
+func (r RoleAssignmentResult) Role(userUUID string) []string {
+	for _, role := range r {
+		if role.IAMUserUUID == userUUID {
+			var roles []string
+			for _, foundRole := range role.Roles {
+				roles = append(roles, foundRole.Role)
+			}
+			return roles
+		}
+	}
+	return []string{}
+}
