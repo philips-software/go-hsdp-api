095f659b04da2ae8728ea5d2f16af2a0ecd1b316
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2023-01-31 06:53:43 +0100

    Don't hardcode page size

--- iam/groups_service.go
+++ iam/groups_service.go
@@ -383,11 +383,13 @@ func (g *GroupsService) SCIMGetGroupByIDAll(id string, opt *SCIMGetGroupOptions,
 	if opt == nil {
 		opt = &SCIMGetGroupOptions{}
 	}
-	count := 100
+	if opt.GroupMembersCount == nil {
+		count := 100 // Max
+		opt.GroupMembersCount = &count
+	}
 	current := 1
 	for {
 		var data *SCIMGroup
-		opt.GroupMembersCount = &count
 		// GroupMembersStartIndex = page number, really..
 		opt.GroupMembersStartIndex = &current
 		data, resp, err = g.SCIMGetGroupByID(id, opt, options...)
@@ -400,7 +402,7 @@ func (g *GroupsService) SCIMGetGroupByIDAll(id string, opt *SCIMGetGroupOptions,
 			scimGroup.ExtensionGroup.GroupMembers.Resources = append(scimGroup.ExtensionGroup.GroupMembers.Resources, data.ExtensionGroup.GroupMembers.Resources...)
 		}
 		current = current + 1
-		if len(scimGroup.ExtensionGroup.GroupMembers.Resources) >= data.ExtensionGroup.GroupMembers.TotalResults || (current*count) > data.ExtensionGroup.GroupMembers.TotalResults {
+		if len(scimGroup.ExtensionGroup.GroupMembers.Resources) >= data.ExtensionGroup.GroupMembers.TotalResults || (*opt.GroupMembersCount*current) > data.ExtensionGroup.GroupMembers.TotalResults {
 			break // Done
 		}
 	}
